c6689764047688329ad4d18a87c2dbd2afa03582
arunikayadav42 <arunikayadav42@gmail.com>
2018-02-27 14:21:14 +0530

    Pizza hut project here

---
+++ javascriptpractice.js
@@ -0,0 +1,8 @@
+function calculateAge(yearOfBirth)
+{
+	var age=2017-yearOfBirth;
+
+	return age;
+}
+var ageJohn=calculateAge(1990);
+console.log(ageJohn);
---
+++ jquerypractice.php
@@ -0,0 +1,25 @@
+
+<html>
+<head lang="en-US">
+	<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
+	
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<p id="paragraph" style="display:none;">This is a paragraph</p>
+	<img id="image" src="https://yt3.ggpht.com/--n5ELY2uT-U/AAAAAAAAAAI/AAAAAAAAAAA/d9JvaIEpstw/s88-c-k-no-mo-rj-c0xffffff/photo.jpg">
+	<iframe src="https://www.google.co.in/?gfe_rd=cr&ei=hpJfWcrWJIKHvATYuoKwDg&gws_rd=ssl"></iframe>
+	<script>
+		$(window).load(function(){
+			alert('Document is loaded');
+		});
+	</script>
+</body>
+</html>
